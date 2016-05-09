5934b73c26be6ad61bc58c571ebaed13c1d8ddd8
kc88 <kc-1988@hotmail.com>
2016-05-08 22:50:21 -0400

    Update something.js

--- something.js
+++ something.js
@@ -18,27 +18,32 @@
   </div>
  
   <script type='text/javascript'>
-kongregate.mtx.addEventListener("adsAvailable", function(e:Event):void{
-  // Ads are now available, calls to kongregate.mtx.showIncentivizedAd() will work
-});
+	function Function1(arg):void
+	{ 
+		alert( arg ); 
+	}
+
+	kongregate.mtx.addEventListener("adsAvailable", function(e:Event):void{
+  		// Ads are now available, calls to kongregate.mtx.showIncentivizedAd() will work
+	});
  
-kongregate.mtx.addEventListener("adsUnavailable", function(e:Event):void{
-  // Ads are no longer available, calls to kongregate.mtx.showIncentivizedAd() will fail
-});
+	kongregate.mtx.addEventListener("adsUnavailable", function(e:Event):void{
+  		// Ads are no longer available, calls to kongregate.mtx.showIncentivizedAd() will fail
+	});
  
-kongregate.mtx.addEventListener("adOpened", function(e:Event):void{
-  // An ad is being displayed
-});
+	kongregate.mtx.addEventListener("adOpened", function(e:Event):void{
+  		// An ad is being displayed
+	});
  
-kongregate.mtx.addEventListener("adCompleted", function(e:Event):void{
-  // An ad has completed successfully, and the player should be rewarded
-});
+	kongregate.mtx.addEventListener("adCompleted", function(e:Event):void{
+  		// An ad has completed successfully, and the player should be rewarded
+	});
  
-kongregate.mtx.addEventListener("adAbandoned", function(e:Event):void{
-  // Ad ad has been closed before completion, the player should not be rewarded
-});
+	kongregate.mtx.addEventListener("adAbandoned", function(e:Event):void{
+  		// Ad ad has been closed before completion, the player should not be rewarded
+	});
  
-kongregate.mtx.initializeIncentivizedAds();
+	kongregate.mtx.initializeIncentivizedAds();
 
   </script>
 </body>
